05d2fdaa20882724dbba075f9eb9f56193aad494
unknown <cehstephen@gmail.com>
2023-03-11 21:10:41 +0000

    Login added

--- rentals/apiviews.py
+++ rentals/apiviews.py
@@ -1,4 +1,8 @@
 from rest_framework import generics
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 
 from .serializers import CarSerializer, EngineTypeSerializer, BatteryTypeSerializer, SerivceCriteriaSerializer, CarTypeSerializer, AllCarsNeedingServicingSeializer, UserSerializer
@@ -57,6 +61,21 @@ class UserCreate(generics.CreateAPIView):
     authentication_classes = ()
     permission_classes = ()
     serializer_class = UserSerializer
+
+class UserLogin(APIView):
+    permission_classes = ()
+
+    def post(self, request,):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+        
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
 
 """
 Django filter documentation
--- rentals/urls.py
+++ rentals/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .apiviews import CarList, CheckCars, EngineTypeList, BatteryTypeList, ServiceCriteriaList, CarDetail, CarTypeList, CarsNeedingServicing, UserCreate
+from .apiviews import CarList, CheckCars, EngineTypeList, BatteryTypeList, ServiceCriteriaList, CarDetail, CarTypeList, CarsNeedingServicing, UserCreate, UserLogin
 
 
 urlpatterns = [
     path("users/create", UserCreate.as_view(), name="users_create"),
+    path("login", UserLogin.as_view(), name="login"),
     path("cars", CarList.as_view(), name="cars"),
     path("engines", EngineTypeList.as_view(), name="engines"),
     path("battery", BatteryTypeList.as_view(), name="batteries"),
